262e66eaa9c3fcc11e8ad94b7e38a261a0f37f73
Evan Machnic <emachnic@broadmac.net>
2012-04-12 08:28:32 -0400

    Separating stuff into different platforms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyApp::Application.config.secret_token = '2f9a32c17563aa92b170830f823f05c7f660910d5a769030e02f08b3bbc867145d217d7e354489fcc3e36611b65f2c7b4a117434a6a7e9384963ff4bcc698801'
